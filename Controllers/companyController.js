f42c12fc4da05808c9d807038dbc6be6e98531eb
vladiyudi <plastikwood@gmail.com>
2022-09-21 14:03:32 +0300

    calcs

--- Controllers/companyController.js
+++ Controllers/companyController.js
@@ -4,6 +4,7 @@ const companyData = require('../Schemas/companySchema')
 exports.updateDB = async (req, res) => {
 const {userid} = req.body
 const {rawdata} = req.body
+const {kpi}=req.body
 console.log(req.body)
 res.send(req.body)
 }
--- Routes/companyRoute.js
+++ Routes/companyRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const companyData = require('../Schemas/companySchema')
 const {updateDB} = require('../Controllers/companyController')
 const {auth} = require('../Middleware/userMiddleware')
+const {getCalculations} = require('../Middleware/company')
 
 
-router.post('/analize', auth, updateDB)
+router.post('/analize', auth, getCalculations, updateDB)
 
 module.exports = router;
