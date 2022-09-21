9d2a0f017992934a637aab345696ec5b6985de55
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-21 20:11:35 +0300

    add connectDB util

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-const express = require('express');
-require('dotenv').config({path:"./.env"})
+const express = require("express");
+require("dotenv").config({ path: "./.env" });
 const app = express();
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const passport = require('passport')
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE, {useNewUrlParser:true,useUnifiedTopology:true})
-const companyData = require('./Schemas/companySchema')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const connectDB = require("./utils/connectDB");
+// mongoose.connect(process.env.DATABASE, {useNewUrlParser:true,useUnifiedTopology:true})
+const companyData = require("./Schemas/companySchema");
 
-const companyRoute = require('./Routes/companyRoute')
-const userRoute = require('./Routes/userRoute')
-require('./Utils/GoogleAuth')
+const companyRoute = require("./Routes/companyRoute");
+const userRoute = require("./Routes/userRoute");
+require("./Utils/GoogleAuth");
 
+app.use(express.json());
+app.use(cors({ origin: process.env.BASE_URL, credentials: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.json())
-app.use(cors({origin: process.env.BASE_URL, credentials: true}))
-app.use(cookieParser())
-app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use("/api/company", companyRoute);
+app.use("/api/user", userRoute);
 
-app.use('/api/company', companyRoute)
-app.use('/api/user', userRoute)
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}.`);
-})
+connectDB((err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  app.listen(process.env.PORT, () => {
+    console.log(`👉 Server listening on ${process.env.PORT}.`);
+  });
+});
